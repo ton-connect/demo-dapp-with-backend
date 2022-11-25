5c790e3906e404753ca2472cfc9e58c4d0c3f00a
siandrev <andreev.sergey.i@yandex.ru>
2022-11-25 16:04:02 +0400

    github pages paths fixes

--- src/connector.ts
+++ src/connector.ts
@@ -3,7 +3,7 @@ import { notification } from 'antd';
 
 // Just to fix Githab pages url problem.
 // If your app main route is hte same as `window.location.origin` you don't have to pass this argument to the TonConnect constructor
-const dappMetadata = { dappMetedata: { url: 'https://ton-connect.github.io/demo-dapp/' } };
+const dappMetadata = { dappMetedata: { url: 'https://ton-connect.github.io/demo-dapp-with-backend/' } };
 
 export const connector = new TonConnect(dappMetadata);
 
@@ -34,80 +34,4 @@ export async function sendTransaction(tx: any): Promise<{ boc: string }> {
 		console.log(e);
 		throw e;
 	}
-}
-
-(window as any).mockTonConnect = mockTonConnect;
-//mockTonConnect();
-
-export function mockTonConnect() {
-	(window as any).tonkeeper = {
-		tonconnect: {
-			listener: undefined,
-			isWalletBrowser: true,
-			disconnect() {},
-			restoreConnection() {
-				return Promise.resolve({
-					event: 'connect',
-					payload: {
-						items: [
-							{
-								name: 'ton_addr',
-								address: '0:412410771DA82CBA306A55FA9E0D43C9D245E38133CB58F1457DFB8D5CD8892F',
-								network: '-239',
-							},
-							{
-								name: 'ton_proof',
-								proof: {
-									timestamp: Math.round(Date.now() / 1000),
-									domain: {
-										lengthBytes: 15,
-										value: 'toncoolswap.com',
-									},
-									payload: 'test_ton_proof 123',
-									signature: '0x' + '1234'.repeat(64),
-								},
-							},
-						],
-						device: {
-							platform: 'iphone',
-							app: 'Tonkeeper',
-							version: '2.7.1',
-						},
-					},
-				});
-			},
-			connect() {
-				return Promise.resolve({
-					event: 'connect',
-					payload: {
-						items: [
-							{
-								name: 'ton_addr',
-								address: '0:412410771DA82CBA306A55FA9E0D43C9D245E38133CB58F1457DFB8D5CD8892F',
-								network: '-239',
-							},
-						],
-						device: {
-							platform: 'iphone',
-							app: 'Tonkeeper',
-							version: '2.7.1',
-						},
-					},
-				});
-			},
-			// @ts-ignore
-			send(req) {
-				console.log('Request received', req);
-				return Promise.resolve({
-					id: req.id,
-					result: 'mocked_boc',
-				});
-			},
-			// @ts-ignore
-			listen(callback) {
-				this.listener = callback;
-				return () => {};
-			},
-		},
-	};
 }
